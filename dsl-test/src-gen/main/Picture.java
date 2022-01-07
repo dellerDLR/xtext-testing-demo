d67e5dd5cc49d1e4af871228deab92c23ad4da35
elle_de <elle_de@SC-010149.intra.dlr.de>
2022-01-07 18:16:02 +0100

    added Picture entity

---
+++ dsl-test/src-gen/main/Picture.java
@@ -0,0 +1,13 @@
+package main;
+		    public class Picture extends common.HasAuthor {
+private String content;
+public String getContent() {
+	return content;
+	}
+public void setContent(String content) {
+this.content = content;
+}
+
+        }
+}
+
